4e8b3b5ed4e166897592918132544a57597136f2
juiceboxjoe <brianlandron@gmail.com>
2020-02-08 22:23:05 -0400

    cleanup.

--- api/routes/appointments/appointmentControllers.js
+++ api/routes/appointments/appointmentControllers.js
@@ -2,7 +2,6 @@ const Promises = require('../../utils/promises')
 const mongoose = require('mongoose');
 const Appointments = mongoose.model('Appointments');
 const CreateAppointmentReqSchema = require('./reqSchemas/createAppointmentReqSchema')
-const Schema = mongoose.Schema;
 
 exports.create  = (req, res) => {
     Promises.hasValidBodyAndSchema(req, res, CreateAppointmentReqSchema)
@@ -18,11 +17,3 @@ exports.create  = (req, res) => {
             return Promises.handleError(req, res, e);
         });
 }
-
-// exports.list = (req, res) => {
-//     appointments.find({}, function(e, blobs) {
-//         if (e)
-//             return Promises.handleError(req, res, e);
-//         return Promises.handleSuccess(req, res, blobs);
-//     });
-// };
